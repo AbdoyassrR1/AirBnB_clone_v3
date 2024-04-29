d175118b3cc4c615f7fa29188e180399e24936a5
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-04-29 18:32:31 +0300

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ flask app """
 
-from flask import Flask
-from models import storage
 from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exception):
     """ call storage.close"""
     storage.close()
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" """
+""" index module"""
 from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route("/status", methods=["GET"])
 def status():
     ''' routes to status page '''
     return jsonify({'status': 'OK'})
