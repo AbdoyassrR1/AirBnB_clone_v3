4eaa188db1e53ee7390ee2ee17ad7ff46a136820
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-05-01 18:42:31 +0300

    added CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,12 @@ from os import getenv
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-
+CORS(app, origins=["0.0.0.0"])
 host = getenv("HBNB_API_HOST", "0.0.0.0")
 port = getenv("HBNB_API_PORT", "5000")
 
